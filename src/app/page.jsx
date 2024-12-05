bd7fb288ed81abc901b262fbcaa0122429731d9a
Nomanpervaiz <nomanpervaiz035@gmail.com>
2024-12-05 21:58:13 +0500

    change import component name

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,5 @@
+import AboutSection from "@/components/AboutSection";
 import Header from "@/components/Header";
-import AboutSection from "@/components/aboutSection";
 import ProjectSection from "@/components/ProjectSection";
 
 function Home() {
